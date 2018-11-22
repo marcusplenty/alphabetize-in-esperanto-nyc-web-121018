990a7714d398184f50fbac0df828d7f209f9d095
Marcus Plenty <marcusplenty@gmail.com>
2018-11-22 00:25:58 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,9 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
+  arr.sort_by do |word|
+    word.split('').map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
+
 end
